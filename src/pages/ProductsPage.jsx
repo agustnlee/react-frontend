f6d0795d763b6b0d6418b299e60e3a4ee8e1391f
agustnlee <lyoonsoo25@gmail.com>
2026-03-06 18:55:23 -0300

    Feature: Set index.css global variables and component classes based on V4

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,7 @@
+export default function ProductsPage() {
+  return (
+    <div className="p-8 text-muted text-sm">
+      Products Page 
+    </div>
+  )
+}
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <div className="p-8 text-muted text-sm">
+      Register Page 
+    </div>
+  )
+}
