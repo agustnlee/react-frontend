e1a2f21e736db660a73fba5676609e1c6b55ceca
agustnlee <lyoonsoo25@gmail.com>
2026-03-08 19:53:12 -0300

    Fix: Typo createAsyncThunk in authSlice.js

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncTrunk } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
 
